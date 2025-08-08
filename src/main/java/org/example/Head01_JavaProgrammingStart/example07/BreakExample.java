bb23de8c0963794a2a109b737b854bd04b930551
전채연 <ami02074657@gmail.com>
2025-08-08 15:23:25 +0900

    무한커밋

---
+++ src/main/java/org/example/Head01_JavaProgrammingStart/example07/BreakExample.java
@@ -0,0 +1,12 @@
+package org.example.Head01_JavaProgrammingStart.example07;
+
+public class BreakExample {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            if(i%2 == 0){
+                System.out.println("i: " + i);
+                break;
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/Head01_JavaProgrammingStart/practice/BreakContinueExample.java
@@ -0,0 +1,13 @@
+package org.example.Head01_JavaProgrammingStart.practice;
+
+public class BreakContinueExample {
+    public static void main(String[] args) {
+        for(int i = 1; i<=5; i++){
+            if(i == 3) continue;
+            if(i == 4) break;
+            System.out.println(i);
+
+            //예상 출력: 1 2
+        }
+    }
+}
